65ca82f13f91851c9c8449749aea3578990164d1
MAHANOOR651 <hiroofcs@gmail.com>
2024-11-24 20:58:02 +0500

    Add files via upload

---
+++ src/app/Components/header.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <div className='bg=white'>
+      {/* Header */}
+      <header className='bg-[#85A88B] p-4'>
+      <div className="max-w-7xl mx-auto flex items-center justify-between">
+          {/* Logo */}
+          <div className="text-white text-3xl font-bold tracking-wider">
+            MANZZARI
+          </div>
+           {/* Navigation Icons */}
+           <div className="flex items-center ">
+            {/* Heart Icon */}
+            <button className="p-2 hover:bg-gray-100 rounded-full transition-colors">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-gray-600 hover:text-red-500">
+                <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+              </svg>
+            </button>
+            
+            {/* User Icon */}
+            <button className="p-2 hover:bg-gray-100 rounded-full transition-colors">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-gray-600">
+                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                <circle cx="12" cy="7" r="4"></circle>
+              </svg>
+            </button>
+
+            {/* Shopping Cart Icon */}
+            <button className="p-2 hover:bg-gray-100 rounded-full transition-colors relative">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-gray-600">
+                <circle cx="9" cy="21" r="1"></circle>
+                <circle cx="20" cy="21" r="1"></circle>
+                <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+              </svg>
+
+            </button>
+        </div>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default header
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from ".//components/Header";
+import Hero from ".//components/hero";
+
+
+export default function page(){
+  return(
+    <div>
+      <Header></Header>
+      <Hero></Hero>
+    </div>
+  )
+}
